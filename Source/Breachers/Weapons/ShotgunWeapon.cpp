c571ada2fd2a39c5f1ebe46636b3499a2e092299
Ali Aboshady <ali_aboshady@hotmail.com>
2022-01-17 15:33:35 +0000

    Added shotgun

---
+++ Source/Breachers/Weapons/ShotgunWeapon.cpp
@@ -0,0 +1,2 @@
+#include "ShotgunWeapon.h"
+
---
+++ Source/Breachers/Weapons/ShotgunWeapon.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "WeaponBase.h"
+#include "ShotgunWeapon.generated.h"
+
+UCLASS()
+class BREACHERS_API AShotgunWeapon : public AWeaponBase
+{
+	GENERATED_BODY()
+	
+};
