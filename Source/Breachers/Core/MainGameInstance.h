43652a8fb9378b4873960d76012887ea618e0580
Ali Aboshady <ali_aboshady@hotmail.com>
2022-01-19 18:29:05 +0000

    Added game instance class

---
+++ Source/Breachers/Core/MainGameInstance.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "MainGameInstance.generated.h"
+
+UCLASS()
+class BREACHERS_API UMainGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+};
