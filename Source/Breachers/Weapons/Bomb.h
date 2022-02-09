6d8dff308b2c952b32d2a94fda00ca75697420d8
Ali Aboshady <ali_aboshady@hotmail.com>
2022-02-09 00:15:20 +0000

    Added bomb weapon

---
+++ Source/Breachers/Weapons/Bomb.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "WeaponBase.h"
+#include "Bomb.generated.h"
+
+UCLASS()
+class BREACHERS_API ABomb : public AWeaponBase
+{
+	GENERATED_BODY()
+	
+};
