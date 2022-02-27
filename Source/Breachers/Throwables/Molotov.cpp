fed15689307b09c169b433bead19642d555aa79b
Ali Aboshady <ali_aboshady@hotmail.com>
2022-02-27 19:44:09 +0000

    Initial molotov logic

--- Source/Breachers/Throwables/Molotov.cpp
+++ Source/Breachers/Throwables/Molotov.cpp
@@ -1,2 +1,27 @@
 #include "Molotov.h"
 
+AMolotov::AMolotov()
+{
+	ActivationSlope = 5;
+}
+
+void AMolotov::BeginPlay()
+{
+	Super::BeginPlay();
+	Mesh_TP->OnComponentHit.AddDynamic(this, &AMolotov::OnHit);
+}
+
+void AMolotov::OnActivate()
+{
+	// Do Something
+	Super::OnActivate();
+}
+
+void AMolotov::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+                     FVector NormalImpulse, const FHitResult& Hit)
+{
+	float Normal_X = NormalImpulse.X;
+	float Normal_Y = NormalImpulse.Y;
+	float sum = Normal_X + Normal_Y;
+	if(sum > -ActivationSlope && sum <= ActivationSlope) OnActivate();
+}
--- Source/Breachers/Throwables/Molotov.h
+++ Source/Breachers/Throwables/Molotov.h
@@ -7,4 +7,17 @@ UCLASS()
 class BREACHERS_API AMolotov : public AThrowableWeapon
 {
 	GENERATED_BODY()
+
+public:
+	AMolotov();
+	
+protected:
+	virtual void BeginPlay() override;
+	virtual void OnActivate() override;
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit );
+
+	UPROPERTY(EditAnywhere)
+	float ActivationSlope;
 };
