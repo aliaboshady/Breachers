f58ee1d70125806c31788d855e8083f914726ba0
Ali Aboshady <ali_aboshady@hotmail.com>
2022-02-27 17:13:39 +0000

    Added initial smoke logic

--- Source/Breachers/Throwables/Smoke.cpp
+++ Source/Breachers/Throwables/Smoke.cpp
@@ -1,2 +1,43 @@
 #include "Smoke.h"
 
+#include "Kismet/KismetSystemLibrary.h"
+
+ASmoke::ASmoke()
+{
+	ActivateAfterTime = 2;
+	StopTimeBeforeActivation = 1;
+	ActivationSpeed = 150;
+}
+
+void ASmoke::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	Server_CheckSpeed();
+	StoppedForTime += DeltaSeconds;
+}
+
+void ASmoke::OnThrow()
+{
+	Super::OnThrow();
+	FTimerHandle ThrownTimerHandle;
+	GetWorldTimerManager().SetTimer(ThrownTimerHandle, this, &ASmoke::SetToThrown, 1, false, 0.5);
+}
+
+void ASmoke::SetToThrown()
+{
+	SetActorTickEnabled(true);
+}
+
+void ASmoke::Server_CheckSpeed_Implementation()
+{
+	if(GetVelocity().Size() <= ActivationSpeed)
+	{
+		if(StoppedForTime >= StopTimeBeforeActivation)
+		{
+			FTimerHandle ActivateTimerHandle;
+			GetWorldTimerManager().SetTimer(ActivateTimerHandle, this, &ASmoke::OnActivate, 1, false, ActivateAfterTime);
+			SetActorTickEnabled(false);
+		}
+	}
+	else StoppedForTime = 0;
+}
--- Source/Breachers/Throwables/Smoke.h
+++ Source/Breachers/Throwables/Smoke.h
@@ -7,4 +7,26 @@ UCLASS()
 class BREACHERS_API ASmoke : public AThrowableWeapon
 {
 	GENERATED_BODY()
+
+public:
+	ASmoke();
+	
+protected:
+	virtual void Tick(float DeltaSeconds) override;
+	virtual void OnThrow() override;
+	void SetToThrown();
+
+	UFUNCTION(Server, Reliable)
+	void Server_CheckSpeed();
+
+	UPROPERTY(EditAnywhere)
+	float ActivateAfterTime;
+
+	UPROPERTY(EditAnywhere)
+	float StopTimeBeforeActivation;
+	
+	UPROPERTY(EditAnywhere)
+	float ActivationSpeed;
+
+	float StoppedForTime;
 };
