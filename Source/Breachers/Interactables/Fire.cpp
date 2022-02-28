272bfcca7c472c3041026d7c409ed682d44403bf
Ali Aboshady <ali_aboshady@hotmail.com>
2022-02-28 21:39:28 +0000

    Added fire and player burn sound effects

--- Source/Breachers/Interactables/Fire.cpp
+++ Source/Breachers/Interactables/Fire.cpp
@@ -1,6 +1,7 @@
 #include "Fire.h"
 
 #include "Breachers/Characters/CharacterBase.h"
+#include "Components/AudioComponent.h"
 #include "Components/BoxComponent.h"
 #include "GameFramework/Character.h"
 #include "Kismet/GameplayStatics.h"
@@ -16,6 +17,9 @@ AFire::AFire()
 	FireParticles = CreateDefaultSubobject<UParticleSystemComponent>("FireParticles");
 	FireParticles->SetupAttachment(RootComponent);
 	FireParticles->SetRelativeLocation(FVector(0, 0, -33));
+	
+	FireSound = CreateDefaultSubobject<UAudioComponent>("FireSound");
+	FireSound->SetupAttachment(RootComponent);
 
 	bReplicates = true;
 	Rate = 1;
@@ -38,9 +42,16 @@ void AFire::Server_DamagePlayers_Implementation()
 {
 	TArray<AActor*> OverlappingActors;
 	BoxCollision->GetOverlappingActors(OverlappingActors, ACharacterBase::StaticClass());
+	if(OverlappingActors.Num() > 0) Multicast_PlayerBurningSound();
 
 	for (AActor* Player : OverlappingActors)
 	{
 		UGameplayStatics::ApplyDamage(Player, Damage, GetInstigator()->Controller, GetOwner(), UDamageType::StaticClass());
 	}
+}
+
+void AFire::Multicast_PlayerBurningSound_Implementation()
+{
+	if(!PlayerBurningSound) return;
+	UGameplayStatics::PlaySoundAtLocation(GetWorld(), PlayerBurningSound, GetActorLocation());
 }
--- Source/Breachers/Interactables/Fire.h
+++ Source/Breachers/Interactables/Fire.h
@@ -3,6 +3,7 @@
 #include "GameFramework/Actor.h"
 #include "Fire.generated.h"
 
+class USoundCue;
 class UBoxComponent;
 
 UCLASS()
@@ -15,17 +16,25 @@ public:
 	void SetDamage(float NewDamage);
 
 protected:
-
 	virtual void BeginPlay() override;
 	
 	UFUNCTION(Server, Reliable)
 	void Server_DamagePlayers();
+
+	UFUNCTION(NetMulticast, Reliable)
+	void Multicast_PlayerBurningSound();
 
 	UPROPERTY(VisibleAnywhere)
 	UBoxComponent* BoxCollision;
 	
 	UPROPERTY(VisibleAnywhere)
 	UParticleSystemComponent* FireParticles;
+
+	UPROPERTY(VisibleAnywhere)
+	UAudioComponent* FireSound;
+
+	UPROPERTY(EditAnywhere)
+	USoundCue* PlayerBurningSound;
 
 	UPROPERTY(EditAnywhere, meta = (ClampMin = "0.01"))
 	float Rate;
